65a900e2d67425bbb4510adaad3b15010cbbad4e
soyaya <davidlovedavid1015@gmail.com>
2020-04-07 17:28:11 +0200

    check it out

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,22 @@
-const covid19ImpactEstimator = (data) => data;
+
+function region(name, avgAge, avgDailyIncomeInUsd, avgDailyIncomePopulation) {
+    this.name = "name";
+    this.avgAge = 19.7;
+    this.avgDailyIncomeInUsd = 5;
+    this.avgDailyIncomePopulation = 0.71
+};
+
+function period(periodType, timeToElapse, reportedCases, population, totalHospitalBeds) {
+    this.periodType = "days",
+        this.timeToElapse = 58,
+        this.reportedCases = 674,
+        this.population = 66622705,
+        this.totalHospitalBeds = 1380614
+}
+
+
+const covid19ImpactEstimator = (inputData) => {
+    return { region, period }
+};
 
 export default covid19ImpactEstimator;
