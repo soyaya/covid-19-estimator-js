8e395724096e7ad3f4b1042f510531b33a9ba53b
soyaya <davidlovedavid1015@gmail.com>
2020-04-08 17:34:49 +0200

    let it pass

--- src/server.js
+++ src/server.js
@@ -1,5 +1,16 @@
 const data = {
-    region: { name: 'Africa', avgAge: 19.7, avgDailyIncomeInUSD: 5, avgDailyIncomePopulation: 0.71 },
-    comp: { periodType: 'days', timeToElapse: 58, reportedCases: 674, population: 66622705, totalHospitalBeds: 1380614 }
+    region: {
+        name: 'Africa',
+        avgAge: 19.7,
+        avgDailyIncomeInUSD: 5,
+        avgDailyIncomePopulation: 0.71
+    },
+    comp: {
+        periodType: 'days',
+        timeToElapse: 58,
+        reportedCases: 674,
+        population: 66622705,
+        totalHospitalBeds: 1380614
+    }
 }
 export default data;
