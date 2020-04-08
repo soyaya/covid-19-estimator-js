b4c268a4c3fd133fe6b610d151c7cf759e60a00c
soyaya <davidlovedavid1015@gmail.com>
2020-04-08 16:31:23 +0200

    checks

--- src/estimator.js
+++ src/estimator.js
@@ -1,22 +1,55 @@
+import { data } from './server'
 
-function region(name, avgAge, avgDailyIncomeInUsd, avgDailyIncomePopulation) {
-    this.name = "name";
-    this.avgAge = 19.7;
-    this.avgDailyIncomeInUsd = 5;
-    this.avgDailyIncomePopulation = 0.71
+const futureTime = data.comp.timeToElapse;
+const dateNow = new Date();
+const nowday = dateNow.getDate();
+const period = ({ nowday, futureTime }) => { return futureTime - nowday }
+const n = maths.round({ period } / 3);
+
+const impact = () => {
+    return {
+        currentlyInfected = ({ reportedCases }) => {
+            return reportedCases * 10
+        }
+    }
 };
 
-function period(periodType, timeToElapse, reportedCases, population, totalHospitalBeds) {
-    this.periodType = "days",
-        this.timeToElapse = 58,
-        this.reportedCases = 674,
-        this.population = 66622705,
-        this.totalHospitalBeds = 1380614
-}
-
-
-const covid19ImpactEstimator = (inputData) => {
-    return { region, period }
+const severeImpact = () => {
+    return {
+        currentlyInfected = ({ reportedCases }) => {
+            return reportedCases * 50
+        }
+    }
 };
+
+const infectionsByRequestedTime1 = ({ n, impact }) => { return (impact * 2 ^ n) };
+
+const impactrate = infectionsByRequestedTime1;
+
+const infectionsByRequestedTime2 = ({ n, severeImpact }) => {
+    return (severeImpact * 2 ^ n)
+};
+
+const severeImpactrate = infectionsByRequestedTime2;
+
+const covid19ImpactEstimator = ({ data, impact, severeImpact }) => {
+
+    return {
+        data: {}, // the input data you got
+        impact: {}, // your best case estimation
+        severeImpact: {} // your severe case estimation
+    }
+};
+//case 1
+//currently infected reported cases*10
+//output impact{currentlyInfected:reportedCases*10}
+//output severeImpact{severeImpact.currentlyInfected is reportedCases * 50)}
+//To estimate the number of infected people 28 days from now, note that currentlyInfected doubles
+//every 3 days, so you'd have to multiply it by a factor of 2.
+//currentlyInfected x (2 to the power of *factor*) where factor is 9 for a 28 day
+//currentlyInfected x (2 to the power of 9)
+//This computation should be saved as infectionsByRequestedTime, and should done both for impact
+//and severeImpact, using their respective currentlyInfected values.
+
 
 export default covid19ImpactEstimator;
