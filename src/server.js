9742a214ed8b256a3292fbedb703824275d65fbc
soyaya <davidlovedavid1015@gmail.com>
2020-04-08 17:02:19 +0200

    see this

--- src/server.js
+++ src/server.js
@@ -1,18 +1,10 @@
-const data =
-{
+const data = {
     region: {
-        name: "Africa",
-        avgAge: 19.7,
-        avgDailyIncomeInUSD: 5,
-        avgDailyIncomePopulation: 0.71
+        name: 'Africa', avgAge: 19.7, avgDailyIncomeInUSD: 5, avgDailyIncomePopulation: 0.71
 
     },
     comp: {
-        periodType: "days",
-        timeToElapse: 58,
-        reportedCases: 674,
-        population: 66622705,
-        totalHospitalBeds: 1380614
+        periodType: 'days', timeToElapse: 58, reportedCases: 674, population: 66622705, totalHospitalBeds: 1380614
     }
 
 
