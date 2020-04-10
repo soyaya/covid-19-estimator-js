61352e492571a64ae5bed7a8695557e222b4bce7
soyaya <davidlovedavid1015@gmail.com>
2020-04-10 15:00:18 +0200

    nicely done

--- src/estimator.js
+++ src/estimator.js
@@ -15,24 +15,36 @@ const data = {
 let days;
 if (data.periodType === 'days') {
     days = data.timeToElapse;
-}
+};
 if (data.periodType === 'week') {
     days = 7 * data.timeToElapse
-}
+};
 if (data.periodType === 'month') { days = 30 * data.timeToElapse };
 
-const n = Math.ceil(days / 3)
+const n = Math.ceil(days / 3);
+
 
 impact.currentlyInfected = data.reportedCases * 10;
 severeImpact.currentlyInfected = data.reportedCases * 50;
-impact.infectionsByRequestedTime = impact.currentlyInfected * 2 ^ n;
-severeImpact.infectionsByRequestedTime = severImpact.currentlyInfected * 2 ^ n;
+impact.infectionsByRequestedTime = impact.currentlyInfected * 2 ** n;
+severeImpact.infectionsByRequestedTime = severImpact.currentlyInfected * 2 ** n;
 
-impact.severeCasesByRequestedTime = 0.15 * impact.infectionsByRequestedTime;
-severeImpact.severeCasesByRequestedTime = 0.15 * severeImpact.infectionsByRequestedTime;
+impact.severeCasesByRequestedTime = math.ceil(0.15 * impact.infectionsByRequestedTime);
+severeImpact.severeCasesByRequestedTime = math.ceil(0.15 * severeImpact.infectionsByRequestedTime);
 
+impact.hospitalBedsByRequestedTime = Math.ceil(impact.severeCasesByRequestedTime - 0.35 * data.totalHospitalBeds);
+severeImpact.hospitalBedsByRequestedTime = Math.ceil(severeImpact.severeCasesByRequestedTime - 0.35 * data.totalHospitalBeds);
 
-const covid19ImpactEstimator = ({ data, impact, severeImpact }) => {
+impact.casesForICUByRequestedTime = 0.05 * impact.infectionsByRequestedTime;
+severeImpact.casesForICUByRequestedTime = 0.05 * severeImpact.infectionsByRequestedTime;
+
+impact.casesForVentilatorsByRequestedTime = 0.02 * impact.infectionsByRequestedTime;
+severeImpact.casesForVentilatorsByRequestedTime = 0.02 * severeImpact.infectionsByRequestedTime;
+
+impact.dollarsInFlight = Math.trunc(impact.infectionsByRequestedTime * avgDailyIncomeInUSD * days);
+severeImpact.dollarsInFlight = Math.trunc(severeImpact.infectionsByRequestedTime * avgDailyIncomeInUSD * days);
+
+const covid19ImpactEstimator = () => {
 
     return {
         data: {}, // the input data you got
@@ -40,16 +52,5 @@ const covid19ImpactEstimator = ({ data, impact, severeImpact }) => {
         severeImpact: {} // your severe case estimation
     }
 };
-//case 1
-//currently infected reported cases*10
-//output impact{currentlyInfected:reportedCases*10}
-//output severeImpact{severeImpact.currentlyInfected is reportedCases * 50)}
-//To estimate the number of infected people 28 days from now, note that currentlyInfected doubles
-//every 3 days, so you'd have to multiply it by a factor of 2.
-//currentlyInfected x (2 to the power of *factor*) where factor is 9 for a 28 day
-//currentlyInfected x (2 to the power of 9)
-//This computation should be saved as infectionsByRequestedTime, and should done both for impact
-//and severeImpact, using their respective currentlyInfected values.
-
 
 export default covid19ImpactEstimator;
