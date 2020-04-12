799de10af0a95f7164d019c0aab3dd40df2a25ca
soyaya <davidlovedavid1015@gmail.com>
2020-04-12 22:56:21 +0200

    hdgaadhs

--- src/estimator.js
+++ src/estimator.js
@@ -1,33 +1,30 @@
-const data;
 let getFactor;
+const factor = () => {
+  if (data.periodType.trim().toLowerCase() === 'days') {
+    getFactor = Math.trunc((data.timeToElapse * 1) / 3);
+  }
+  else if (data.periodType.trim().toLowerCase() === 'weeks') {
+    getFactor = Math.trunc((data.timeToElapse * 7) / 3);
+  }
+  else if (data.periodType.trim().toLowerCase() === 'weeks') {
+    getFactor = Math.trunc((data.timeToElapse * 30) / 3);
+  }
+  else {
+    getFactor = 1;
+  }
+  return getFactor;
+};
+impact.currentlyInfected = data.reportedCases * 10;
+const trunk = () => {
+  return currentlyInfected * 2 ** factor;
+};
+impact.infectionsByRequestedTime = trunk;
+severeImpact.currentlyInfected = data.reportedCases * 50;
+const trap = () => {
+  return currentlyInfected2 * 2 ** factor;
+};
+severeImpact.infectionsByRequestedTime = trap;
 const covid19ImpactEstimator = () => {
-  const factor = () => {
-    if (data.periodType.trim().toLowerCase() === 'days') {
-      getFactor = Math.trunc((data.timeToElapse * 1) / 3);
-    }
-    else if (data.periodType.trim().toLowerCase() === 'weeks') {
-      getFactor = Math.trunc((data.timeToElapse * 7) / 3);
-    }
-    else if (data.periodType.trim().toLowerCase() === 'weeks') {
-      getFactor = Math.trunc((data.timeToElapse * 30) / 3);
-    }
-    else {
-      getFactor = 1;
-    }
-    return getFactor;
-  };
-  const impact = {};
-  const severeImpact = {};
-  const currentlyInfected1 = data.reportedCases * 10;
-  const trunk = () => {
-    return currentlyInfected1 * 2 ** factor;
-  };
-  impact.currentlyInfected = trunk;
-  const currentlyInfected2 = data.reportedCases * 50;
-  const trap = () => {
-    return currentlyInfected2 * 2 ** factor;
-  };
-  severeImpact.currentlyInfected = trap;
   return {
     data: {
       impact: {
