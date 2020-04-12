3a4fd915c22a1cfacd2b401c246f66e901cefa38
soyaya <davidlovedavid1015@gmail.com>
2020-04-12 22:08:09 +0200

    kljhaxvhjads

--- src/estimator.js
+++ src/estimator.js
@@ -1,20 +1,7 @@
-
+const data;
+let getFactor;
 const covid19ImpactEstimator = () => {
-  const data = {
-    region: {
-      name: 'Africa',
-      avgAge: 19.7,
-      avgDailyIncomeInUSD: 5,
-      avgDailyIncomePopulation: 0.71
-    },
-    periodType: 'days',
-    timeToElapse: 58,
-    reportedCases: 674,
-    population: 66622705,
-    totalHospitalBeds: 1380614
-  };
   const factor = () => {
-    let getFactor;
     if (data.periodType.trim().toLowerCase() === 'days') {
       getFactor = Math.trunc((data.timeToElapse * 1) / 3);
     }
